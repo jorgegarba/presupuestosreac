3461403f766e5f7298a611be8acddde2cbc73b45
Jorge Garnica Blanco <jorgegarba@gmail.com>
2019-05-23 15:12:38 -0500

    Proyecto finalizado V1

---
+++ precache-manifest.ca6e241042dddc0cbae998e97fab6566.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eec5438c34b67fc7fd57b9c4448d41e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9c4cbab2ffa3ecf9657",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "92625e8f421b478da1a1",
+    "url": "/static/js/2.5ca6e9a6.chunk.js"
+  },
+  {
+    "revision": "b9c4cbab2ffa3ecf9657",
+    "url": "/static/js/main.290feab9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
